fa310fea2e4ce00e906b12eeacda7d1fee02b7da
hpar461 <68981068+hpar461@users.noreply.github.com>
2020-09-09 23:59:25 +1200

    Designed Games Module Menu view and partially implemented its controller

---
+++ quinzical/GamesModule/MenuController.java
@@ -0,0 +1,35 @@
+package quinzical.GamesModule;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuController implements Initializable {
+
+    public Button PlayGameButton;
+    public Button ResetGameButton;
+    public Button ReturnToMainMenuButton;
+    public Label UserScoreLabel;
+    public Label BestScoreLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Update UserScoreLabel and BestScoreLabel with corresponding values.
+    }
+
+    public void handlePlayGameButtonAction() {
+
+    }
+
+    public void handleResetGameButtonAction() {
+
+    }
+
+    public void ReturnToMainMenuButtonAction() {
+
+    }
+
+}
