8a4cda1fa6014904a0f592755b07d6c68b5cc3ef
Danil Linkov <[danil.linkov@gmail.com]>
2020-10-24 21:45:38 +1300

    added confetti to reward screen

--- quinzical/GamesModule/EndGame/EndGameController.java
+++ quinzical/GamesModule/EndGame/EndGameController.java
@@ -4,11 +4,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import quinzical.GamesModule.GameManager;
 import quinzical.GamesModule.GamesMenuController;
 import quinzical.GamesModule.ScoreBoard.ScoreBoardManager;
 
+import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ResourceBundle;
 
 public class EndGameController implements Initializable {
@@ -16,6 +21,7 @@ public class EndGameController implements Initializable {
     public TextField nameTextField;
     public Button submitButton;
     public Button dontSaveButton;
+    public ImageView rewardImage;
 
     private static EndGameController _instance;
     private GameManager gameManager = GameManager.getInstance();
@@ -34,11 +40,13 @@ public class EndGameController implements Initializable {
     private void setScoreLabel() {
         int playerScore = GameManager.getInstance().getCurrentScore();
         if(playerScore > ScoreBoardManager.getInstance().getBestScore()) {
+            rewardImage.setImage(new Image(this.getClass().getResource("/quinzical/GamesModule/EndGame/RewardGifs/best.gif").toExternalForm()));
             scoreLabel.setText("Congratulations on completing the game!\n You have achieved a new high score of $" + playerScore + "!" +
                     "\nPlease enter your name and hit submit if you would like to save your score." +
                     "\nOtherwise you could click Don't Save Score.");
         }
         else {
+            rewardImage.setImage(new Image(this.getClass().getResource("/quinzical/GamesModule/EndGame/RewardGifs/notBest.gif").toExternalForm()));
             scoreLabel.setText("Congratulations on completing the game!\n You have achieved a score of $" + playerScore + "!" +
                     "\nPlease enter your name and hit submit if you would like to save your score." +
                     "\nOtherwise you could click Don't Save Score.");
